f1aa2c8f11797dfcff96103e15c09948c7ea05a9
cfwest86 <81761879+cfwest86@users.noreply.github.com>
2021-09-16 12:11:41 -0400

    Add files via upload

--- JoinQuery.sql
+++ JoinQuery.sql
@@ -15,15 +15,32 @@ SELECT  po.county,
 		h.lowtiermedianhomevalue,
 		h.hightiermedianhomevalue,
 		h.singlefamilymedianhomevalue,
-		r.value_inches
+		r.value_inches,
+		c.murder,
+		c.rape,
+		c.robbery,
+		c.Aggravated_assault,
+		c.Burglary,
+		c.Larceny,
+		c.Motor_vehicle_theft,
+		c.Index_per_100k,
+		e.Employment,
+		e.Unemployment,
+		e.Labor_force,
+		e.Unemployment_rate
+
+
 		
 INTO master_data
 FROM population AS po
     INNER JOIN wages AS w
-        ON (po.county = w.county)AND(po.year = w.year)
+        ON ((po.county = w.county)AND(po.year = w.year))
     INNER JOIN housing AS h
-        ON (po.county = h.county)AND(po.year = h.year)
+        ON ((po.county = h.county)AND(po.year = h.year))
 	INNER JOIN rainfall AS r
-		ON (po.county = r.county)AND(po.year = r.year);
-		
+		ON ((po.county = r.county)AND(po.year = r.year))
+	INNER JOIN Crime as c
+		ON ((po.county = c.county)AND(po.year = c.year))
+	INNER JOIN Employment as e
+		ON ((po.county = e.county)AND(po.year = e.year))
 Select * FROM master_data;
--- schema.sql
+++ schema.sql
@@ -14,18 +14,42 @@ LowTierMedianHomeValue INT,
 HighTierMedianHomeValue INT,
 SingleFamilyMedianHomeValue INT,
 FOREIGN KEY (County, YEAR) REFERENCES Population (County, YEAR),
-PRIMARY KEY (County,Year))
+PRIMARY KEY (County,Year));
 	
 CREATE TABLE Rainfall (
 County VARCHAR NOT NULL,
 Year INT NOT NULL,	
 Value_inches DECIMAL,
 FOREIGN KEY (County, YEAR) REFERENCES Population (County, YEAR),
-PRIMARY KEY (County,Year))
+PRIMARY KEY (County,Year));
 
 CREATE TABLE Wages (
 County VARCHAR NOT NULL,
 Year INT NOT NULL,	
 Average_yearly_wage INT,
 FOREIGN KEY (County, YEAR) REFERENCES Population (County, YEAR),
-PRIMARY KEY (County,Year))
+PRIMARY KEY (County,Year));
+
+CREATE TABLE Employment (
+County VARCHAR NOT NULL,
+Year INT NOT NULL,	
+Employment INT,
+Labor_force INT,
+Unemployment INT,
+Unemployment_rate DECIMAL,
+FOREIGN KEY (County, YEAR) REFERENCES Population (County, YEAR),
+PRIMARY KEY (County,Year));
+
+CREATE TABLE Crime (
+County VARCHAR NOT NULL,
+Year INT NOT NULL,	
+Murder INT,
+Rape INT,
+Robbery INT,
+Aggravated_assault INT,
+Burglary INT,
+Larceny INT,
+Motor_vehicle_theft INT,
+Index_per_100k INT,
+FOREIGN KEY (County, YEAR) REFERENCES Population (County, YEAR),
+PRIMARY KEY (County,Year));
